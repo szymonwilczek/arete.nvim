f6f6d9141cdd092ac0b3ef24733b4a453f9ec4cb
Szymon Wilczek <swilczek.lx@gmail.com>
2026-05-22 19:00:53 +0200

    feat(ef): import missing Emacs theme tables
    Imported ef-fig and ef-orange from the local Emacs ef-themes package.
    
    These themes are not present in the local ef-themes.nvim source used for
    the first Ef import, so they are generated through the shared Emacs
    extraction path instead. Keeping them in a separate commit makes the
    provenance clear: most Ef themes mirror the Neovim port, while these
    fill newer upstream gaps from Emacs.
    
    For users, the Ef family now includes the additional dark fig variant
    and the light orange variant as static arete theme tables.
    
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

---
+++ lua/arete/themes/ef/ef-fig.lua
@@ -0,0 +1,344 @@
+return {
+	["background"] = "dark",
+	["highlights"] = {
+		["Normal"] = {
+			["fg"] = "#e4d3e1",
+			["bg"] = "#3b2043",
+		},
+		["NormalFloat"] = {
+			["fg"] = "#e4d3e1",
+			["bg"] = "#6a586e",
+		},
+		["FloatBorder"] = {
+			["fg"] = "#796f6e",
+		},
+		["Cursor"] = {
+			["bg"] = "#e0646a",
+		},
+		["CursorLine"] = {
+			["bg"] = "#47275f",
+		},
+		["ColorColumn"] = {
+			["fg"] = "#6a586e",
+			["bg"] = "#6a586e",
+		},
+		["Visual"] = {
+			["bg"] = "#643270",
+		},
+		["VisualNOS"] = {
+			["fg"] = "#e4d3e1",
+			["bg"] = "#6a4f6f",
+		},
+		["Search"] = {
+			["fg"] = "#e4d3e1",
+			["bg"] = "#55421e",
+		},
+		["IncSearch"] = {
+			["fg"] = "#e4d3e1",
+			["bg"] = "#847020",
+		},
+		["CurSearch"] = {
+			["fg"] = "#e4d3e1",
+			["bg"] = "#847020",
+		},
+		["Substitute"] = {
+			["fg"] = "#e4d3e1",
+			["bg"] = "#b23f30",
+		},
+		["LineNr"] = {
+			["fg"] = "#9d9d9d",
+		},
+		["CursorLineNr"] = {
+			["fg"] = "#aad05a",
+			["bold"] = true,
+		},
+		["SignColumn"] = {
+			["fg"] = "#e4d3e1",
+		},
+		["FoldColumn"] = {
+			["fg"] = "#e4d3e1",
+		},
+		["Folded"] = {
+			["fg"] = "#9d9d9d",
+		},
+		["StatusLine"] = {
+			["fg"] = "#c8c597",
+			["bg"] = "#57602e",
+			["underline"] = true,
+		},
+		["StatusLineNC"] = {
+			["fg"] = "#9d9d9d",
+			["bg"] = "#4a385e",
+			["underline"] = true,
+		},
+		["TabLine"] = {
+			["bg"] = "#4a385e",
+		},
+		["TabLineFill"] = {
+			["bg"] = "#4a385e",
+		},
+		["TabLineSel"] = {
+			["bg"] = "#3b2043",
+			["bold"] = true,
+		},
+		["VertSplit"] = {
+			["fg"] = "#796f6e",
+		},
+		["WinSeparator"] = {
+			["fg"] = "#796f6e",
+		},
+		["Directory"] = {
+			["fg"] = "#aad05a",
+		},
+		["Title"] = {
+			["fg"] = "#eec27f",
+		},
+		["MoreMsg"] = {
+			["fg"] = "#7fbb3f",
+			["bold"] = true,
+		},
+		["WarningMsg"] = {
+			["fg"] = "#e4c53f",
+			["bold"] = true,
+		},
+		["ErrorMsg"] = {
+			["fg"] = "#ef797f",
+			["bold"] = true,
+		},
+		["Error"] = {
+			["fg"] = "#ef797f",
+			["bold"] = true,
+		},
+		["Todo"] = {
+			["fg"] = "#e4c53f",
+			["bold"] = true,
+		},
+		["NonText"] = {
+			["fg"] = "#9d9d9d",
+		},
+		["SpecialKey"] = {
+			["fg"] = "#7fbb3f",
+			["bold"] = true,
+		},
+		["PmenuSbar"] = {
+			["fg"] = "#796f6e",
+			["bg"] = "#3b2043",
+		},
+		["PmenuThumb"] = {
+			["fg"] = "#e4d3e1",
+			["bg"] = "#7f5a60",
+		},
+		["Comment"] = {
+			["fg"] = "#9d9d9d",
+			["italic"] = true,
+		},
+		["Constant"] = {
+			["fg"] = "#afb1ff",
+		},
+		["String"] = {
+			["fg"] = "#e088af",
+		},
+		["Character"] = {
+			["fg"] = "#e088af",
+		},
+		["Number"] = {
+			["fg"] = "#afb1ff",
+		},
+		["Boolean"] = {
+			["fg"] = "#afb1ff",
+		},
+		["Float"] = {
+			["fg"] = "#afb1ff",
+		},
+		["Identifier"] = {
+			["fg"] = "#f2a85f",
+		},
+		["Function"] = {
+			["fg"] = "#eec27f",
+		},
+		["Statement"] = {
+			["fg"] = "#aad05a",
+			["bold"] = true,
+		},
+		["Conditional"] = {
+			["fg"] = "#aad05a",
+			["bold"] = true,
+		},
+		["Repeat"] = {
+			["fg"] = "#aad05a",
+			["bold"] = true,
+		},
+		["Label"] = {
+			["fg"] = "#aad05a",
+			["bold"] = true,
+		},
+		["Operator"] = {
+			["fg"] = "#e4d3e1",
+		},
+		["Keyword"] = {
+			["fg"] = "#aad05a",
+			["bold"] = true,
+		},
+		["Exception"] = {
+			["fg"] = "#aad05a",
+			["bold"] = true,
+		},
+		["PreProc"] = {
+			["fg"] = "#ff7f5f",
+		},
+		["Include"] = {
+			["fg"] = "#ff7f5f",
+		},
+		["Define"] = {
+			["fg"] = "#ff7f5f",
+		},
+		["Macro"] = {
+			["fg"] = "#ff7f5f",
+		},
+		["PreCondit"] = {
+			["fg"] = "#ff7f5f",
+		},
+		["Type"] = {
+			["fg"] = "#79d081",
+			["bold"] = true,
+		},
+		["StorageClass"] = {
+			["fg"] = "#79d081",
+			["bold"] = true,
+		},
+		["Structure"] = {
+			["fg"] = "#79d081",
+			["bold"] = true,
+		},
+		["Typedef"] = {
+			["fg"] = "#79d081",
+			["bold"] = true,
+		},
+		["Special"] = {
+			["fg"] = "#ffb2d6",
+			["bold"] = true,
+		},
+		["SpecialChar"] = {
+			["fg"] = "#ef797f",
+		},
+		["Tag"] = {
+			["fg"] = "#c59fcf",
+			["italic"] = true,
+		},
+		["Delimiter"] = {
+			["fg"] = "#e4d3e1",
+		},
+		["SpecialComment"] = {
+			["fg"] = "#c59fcf",
+			["italic"] = true,
+		},
+		["Debug"] = {
+			["fg"] = "#e4c53f",
+			["bold"] = true,
+		},
+		["Underlined"] = {
+			["fg"] = "#ffb2d6",
+			["underline"] = true,
+		},
+		["Ignore"] = {
+			["fg"] = "#9d9d9d",
+		},
+		["@comment"] = {
+			["link"] = "Comment",
+		},
+		["@constant"] = {
+			["link"] = "Constant",
+		},
+		["@constant.builtin"] = {
+			["link"] = "Special",
+		},
+		["@constructor"] = {
+			["link"] = "Special",
+		},
+		["@function"] = {
+			["link"] = "Function",
+		},
+		["@function.builtin"] = {
+			["link"] = "Special",
+		},
+		["@keyword"] = {
+			["link"] = "Keyword",
+		},
+		["@keyword.conditional"] = {
+			["link"] = "Conditional",
+		},
+		["@keyword.repeat"] = {
+			["link"] = "Repeat",
+		},
+		["@keyword.exception"] = {
+			["link"] = "Exception",
+		},
+		["@label"] = {
+			["link"] = "Label",
+		},
+		["@module"] = {
+			["link"] = "Include",
+		},
+		["@number"] = {
+			["link"] = "Number",
+		},
+		["@operator"] = {
+			["link"] = "Operator",
+		},
+		["@property"] = {
+			["link"] = "Identifier",
+		},
+		["@punctuation"] = {
+			["link"] = "Delimiter",
+		},
+		["@string"] = {
+			["link"] = "String",
+		},
+		["@tag"] = {
+			["link"] = "Tag",
+		},
+		["@type"] = {
+			["link"] = "Type",
+		},
+		["@variable"] = {
+			["link"] = "Identifier",
+		},
+		["@variable.builtin"] = {
+			["link"] = "Special",
+		},
+		["DiagnosticError"] = {
+			["link"] = "Error",
+		},
+		["DiagnosticWarn"] = {
+			["link"] = "WarningMsg",
+		},
+		["DiagnosticInfo"] = {
+			["link"] = "MoreMsg",
+		},
+		["DiagnosticHint"] = {
+			["link"] = "Question",
+		},
+		["DiagnosticOk"] = {
+			["link"] = "MoreMsg",
+		},
+	},
+	["name"] = "ef-fig",
+	["terminal"] = {
+		[0] = "#3b2043",
+		[1] = "#ef797f",
+		[2] = "#7fbb3f",
+		[3] = "#e4c53f",
+		[4] = "#57aff6",
+		[5] = "#ffb2d6",
+		[6] = "#5fc0dc",
+		[7] = "#9d9d9d",
+		[8] = "#6a586e",
+		[9] = "#ff7f5f",
+		[10] = "#79d081",
+		[11] = "#f2a85f",
+		[12] = "#78b2ff",
+		[13] = "#afb1ff",
+		[14] = "#80dfbf",
+		[15] = "#e4d3e1",
+	},
+}
---
+++ lua/arete/themes/ef/ef-orange.lua
@@ -0,0 +1,344 @@
+return {
+	["background"] = "light",
+	["highlights"] = {
+		["Normal"] = {
+			["fg"] = "#6c4631",
+			["bg"] = "#ffedc9",
+		},
+		["NormalFloat"] = {
+			["fg"] = "#6c4631",
+			["bg"] = "#c9b088",
+		},
+		["FloatBorder"] = {
+			["fg"] = "#cfbe9b",
+		},
+		["Cursor"] = {
+			["bg"] = "#cf5f00",
+		},
+		["CursorLine"] = {
+			["bg"] = "#ffdaa6",
+		},
+		["ColorColumn"] = {
+			["fg"] = "#c9b088",
+			["bg"] = "#c9b088",
+		},
+		["Visual"] = {
+			["bg"] = "#eeb48f",
+		},
+		["VisualNOS"] = {
+			["fg"] = "#6c4631",
+			["bg"] = "#ebdc8f",
+		},
+		["Search"] = {
+			["fg"] = "#6c4631",
+			["bg"] = "#ecdc7a",
+		},
+		["IncSearch"] = {
+			["fg"] = "#6c4631",
+			["bg"] = "#efcf00",
+		},
+		["CurSearch"] = {
+			["fg"] = "#6c4631",
+			["bg"] = "#efcf00",
+		},
+		["Substitute"] = {
+			["fg"] = "#6c4631",
+			["bg"] = "#f3a058",
+		},
+		["LineNr"] = {
+			["fg"] = "#6f6a70",
+		},
+		["CursorLineNr"] = {
+			["fg"] = "#c13f00",
+			["bold"] = true,
+		},
+		["SignColumn"] = {
+			["fg"] = "#6c4631",
+		},
+		["FoldColumn"] = {
+			["fg"] = "#6c4631",
+		},
+		["Folded"] = {
+			["fg"] = "#6f6a70",
+		},
+		["StatusLine"] = {
+			["fg"] = "#742f18",
+			["bg"] = "#ffc255",
+			["underline"] = true,
+		},
+		["StatusLineNC"] = {
+			["fg"] = "#6f6a70",
+			["bg"] = "#e9d0a8",
+			["underline"] = true,
+		},
+		["TabLine"] = {
+			["bg"] = "#e9d0a8",
+		},
+		["TabLineFill"] = {
+			["bg"] = "#e9d0a8",
+		},
+		["TabLineSel"] = {
+			["bg"] = "#ffedc9",
+			["bold"] = true,
+		},
+		["VertSplit"] = {
+			["fg"] = "#cfbe9b",
+		},
+		["WinSeparator"] = {
+			["fg"] = "#cfbe9b",
+		},
+		["Directory"] = {
+			["fg"] = "#c13f00",
+		},
+		["Title"] = {
+			["fg"] = "#cf2030",
+		},
+		["MoreMsg"] = {
+			["fg"] = "#5f7200",
+			["bold"] = true,
+		},
+		["WarningMsg"] = {
+			["fg"] = "#a05a22",
+			["bold"] = true,
+		},
+		["ErrorMsg"] = {
+			["fg"] = "#c13f00",
+			["bold"] = true,
+		},
+		["Error"] = {
+			["fg"] = "#c13f00",
+			["bold"] = true,
+		},
+		["Todo"] = {
+			["fg"] = "#a05a22",
+			["bold"] = true,
+		},
+		["NonText"] = {
+			["fg"] = "#6f6a70",
+		},
+		["SpecialKey"] = {
+			["fg"] = "#c13f00",
+			["bold"] = true,
+		},
+		["PmenuSbar"] = {
+			["fg"] = "#cfbe9b",
+			["bg"] = "#ffedc9",
+		},
+		["PmenuThumb"] = {
+			["fg"] = "#6c4631",
+			["bg"] = "#c0e47f",
+		},
+		["Comment"] = {
+			["fg"] = "#6f6a70",
+			["italic"] = true,
+		},
+		["Constant"] = {
+			["fg"] = "#a05a22",
+		},
+		["String"] = {
+			["fg"] = "#5f7200",
+		},
+		["Character"] = {
+			["fg"] = "#5f7200",
+		},
+		["Number"] = {
+			["fg"] = "#a05a22",
+		},
+		["Boolean"] = {
+			["fg"] = "#a05a22",
+		},
+		["Float"] = {
+			["fg"] = "#a05a22",
+		},
+		["Identifier"] = {
+			["fg"] = "#2f783f",
+		},
+		["Function"] = {
+			["fg"] = "#cf2030",
+		},
+		["Statement"] = {
+			["fg"] = "#c13f00",
+			["bold"] = true,
+		},
+		["Conditional"] = {
+			["fg"] = "#c13f00",
+			["bold"] = true,
+		},
+		["Repeat"] = {
+			["fg"] = "#c13f00",
+			["bold"] = true,
+		},
+		["Label"] = {
+			["fg"] = "#c13f00",
+			["bold"] = true,
+		},
+		["Operator"] = {
+			["fg"] = "#6c4631",
+		},
+		["Keyword"] = {
+			["fg"] = "#c13f00",
+			["bold"] = true,
+		},
+		["Exception"] = {
+			["fg"] = "#c13f00",
+			["bold"] = true,
+		},
+		["PreProc"] = {
+			["fg"] = "#3f6fa2",
+		},
+		["Include"] = {
+			["fg"] = "#3f6fa2",
+		},
+		["Define"] = {
+			["fg"] = "#3f6fa2",
+		},
+		["Macro"] = {
+			["fg"] = "#3f6fa2",
+		},
+		["PreCondit"] = {
+			["fg"] = "#3f6fa2",
+		},
+		["Type"] = {
+			["fg"] = "#aa4f30",
+			["bold"] = true,
+		},
+		["StorageClass"] = {
+			["fg"] = "#aa4f30",
+			["bold"] = true,
+		},
+		["Structure"] = {
+			["fg"] = "#aa4f30",
+			["bold"] = true,
+		},
+		["Typedef"] = {
+			["fg"] = "#aa4f30",
+			["bold"] = true,
+		},
+		["Special"] = {
+			["fg"] = "#ba2d2f",
+			["bold"] = true,
+		},
+		["SpecialChar"] = {
+			["fg"] = "#c13f00",
+		},
+		["Tag"] = {
+			["fg"] = "#a04450",
+			["italic"] = true,
+		},
+		["Delimiter"] = {
+			["fg"] = "#6c4631",
+		},
+		["SpecialComment"] = {
+			["fg"] = "#527545",
+			["italic"] = true,
+		},
+		["Debug"] = {
+			["fg"] = "#a05a22",
+			["bold"] = true,
+		},
+		["Underlined"] = {
+			["fg"] = "#5f7200",
+			["underline"] = true,
+		},
+		["Ignore"] = {
+			["fg"] = "#6f6a70",
+		},
+		["@comment"] = {
+			["link"] = "Comment",
+		},
+		["@constant"] = {
+			["link"] = "Constant",
+		},
+		["@constant.builtin"] = {
+			["link"] = "Special",
+		},
+		["@constructor"] = {
+			["link"] = "Special",
+		},
+		["@function"] = {
+			["link"] = "Function",
+		},
+		["@function.builtin"] = {
+			["link"] = "Special",
+		},
+		["@keyword"] = {
+			["link"] = "Keyword",
+		},
+		["@keyword.conditional"] = {
+			["link"] = "Conditional",
+		},
+		["@keyword.repeat"] = {
+			["link"] = "Repeat",
+		},
+		["@keyword.exception"] = {
+			["link"] = "Exception",
+		},
+		["@label"] = {
+			["link"] = "Label",
+		},
+		["@module"] = {
+			["link"] = "Include",
+		},
+		["@number"] = {
+			["link"] = "Number",
+		},
+		["@operator"] = {
+			["link"] = "Operator",
+		},
+		["@property"] = {
+			["link"] = "Identifier",
+		},
+		["@punctuation"] = {
+			["link"] = "Delimiter",
+		},
+		["@string"] = {
+			["link"] = "String",
+		},
+		["@tag"] = {
+			["link"] = "Tag",
+		},
+		["@type"] = {
+			["link"] = "Type",
+		},
+		["@variable"] = {
+			["link"] = "Identifier",
+		},
+		["@variable.builtin"] = {
+			["link"] = "Special",
+		},
+		["DiagnosticError"] = {
+			["link"] = "Error",
+		},
+		["DiagnosticWarn"] = {
+			["link"] = "WarningMsg",
+		},
+		["DiagnosticInfo"] = {
+			["link"] = "MoreMsg",
+		},
+		["DiagnosticHint"] = {
+			["link"] = "Question",
+		},
+		["DiagnosticOk"] = {
+			["link"] = "MoreMsg",
+		},
+	},
+	["name"] = "ef-orange",
+	["terminal"] = {
+		[0] = "#6c4631",
+		[1] = "#ba2d2f",
+		[2] = "#007a0a",
+		[3] = "#a05a22",
+		[4] = "#375cc6",
+		[5] = "#ba3e54",
+		[6] = "#467080",
+		[7] = "#c9b088",
+		[8] = "#6f6a70",
+		[9] = "#c13f00",
+		[10] = "#2f783f",
+		[11] = "#b05115",
+		[12] = "#5f50df",
+		[13] = "#8448aa",
+		[14] = "#0f738f",
+		[15] = "#ffedc9",
+	},
+}
